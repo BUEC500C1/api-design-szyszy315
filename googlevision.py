2bd8576b9c0eb2c7ad74179a970f04f78ebc6690
szyszy315 <zeyusong@bu.edu>
2020-02-01 19:32:01 -0500

    Create googlevision.py

---
+++ googlevision.py
@@ -0,0 +1,25 @@
+import getpicture
+import json
+from google.cloud import vision
+
+def getdescription(tweets):
+  image_uri = tweets
+  for i in image_uri:
+    client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
+    image.source.image_uri = i
+
+    response = client.label_detection(image=image)
+
+    print('Labels (and confidence score):')
+    print('=' * 79)
+    for label in response.label_annotations:
+      print(f'{label.description} ({label.score*100.:.2f}%)')
+
+
+if __name__ == '__main__':
+#get tweets
+  tweets = []
+  with open('tweet.json') as file:
+    tweets = json.load(file)
+  getdescription(tweets)
