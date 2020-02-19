7bf8d74edb712b80e45211608768edabb6566674
szyszy315 <zeyusong@bu.edu>
2020-02-19 17:31:02 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import json
+import ppy
 
 def test():
   tweets = []
@@ -10,7 +11,17 @@ def test():
       continue
     else:
       print('error')
+      return
   print("all done")
+
+def content():
+  aa = ppy("@BU_Tweets")
+  if len(aa) == 0:
+    print("error")
+    return
+  print("content is right")
+
 
 if __name__ == '__main__':
   test()
+  content();
